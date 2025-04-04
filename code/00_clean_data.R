c300c627faa2fe6edbf71533f9e03904ea8d0d46
Angela Yu <angela.yu@emory.edu>
2025-04-03 22:08:07 -0400

    Clean the data

---
+++ code/00_clean_data.R
@@ -0,0 +1,14 @@
+here::i_am("code/Final_Project_RMarkdown.Rmd")
+absolute_path_to_data <- here::here("data", "filtered_data.csv")
+dataset <- read.csv(absolute_path_to_data)
+dataset <- dataset[dataset$activityID %in% c("lying", "sitting", "standing", "walking"), ]
+library(dplyr)
+
+dataset <- dataset %>%
+  group_by(activityID) %>%
+  mutate(Time = row_number()) %>%
+  ungroup()
+saveRDS(
+  data, 
+  file = here::here("output/data_clean.rds")
+)
