3b8261f863c7d23beead51994750ba0fafd95368
Angela Yu <angela.yu@emory.edu>
2025-05-03 11:05:13 -0400

    Update

--- code/00_clean_data.R
+++ code/00_clean_data.R
@@ -1,4 +1,6 @@
-here::i_am("Final_Project_RMarkdown.Rmd")
+if (!require("here")) install.packages("here", repos = "http://cran.us.r-project.orgLinks to an external site.")
+library(here)
+here::i_am("code/00_clean_data.R")
 absolute_path_to_data <- here::here("data", "filtered_data.csv")
 dataset <- read.csv(absolute_path_to_data)
 dataset <- dataset[dataset$activityID %in% c("lying", "sitting", "standing", "walking"), 1:2]
